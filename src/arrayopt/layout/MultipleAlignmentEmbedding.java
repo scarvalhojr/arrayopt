4afbf9ac82a029a41916e6f903224f3ecc568455
scarvalh <>
2005-12-01 19:10:20 +0000

    Added a new class MultipleAlignmentEmbedding, subclass of ProbeEmbeddingAlgorithm that will reembed probes based on some sort of multiple alignment of the probes

---
+++ src/arrayopt/layout/MultipleAlignmentEmbedding.java
@@ -0,0 +1,68 @@
+/*
+ * MultipleAlignmentEmbedding.java
+ *
+ * $Revision$
+ *
+ * $Date$
+ *
+ * Copyright 2005 Sergio Anibal de Carvalho Junior
+ *
+ * This file is part of ArrayOpt.
+ *
+ * --- License ----------------------------------------------------------------
+ * ArrayOpt is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * ÂrrayOpt is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * ArrayOpt; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307, USA.
+ * ----------------------------------------------------------------------------
+ *
+ * This is the result of a PhD work developed at the Universitaet Bielefeld
+ * under the supervision of Dr. Sven Rahmann. Proper attribution of the author
+ * as the source of the software is appreciated.
+ *
+ * Sergio Anibal de Carvalho Jr.  http://www.cebitec.uni-bielefeld.de/~scarvalh
+ * AG Genominformatik             http://gi.cebitec.uni-bielefeld.de
+ * Universitaet Bielefeld         http://www.uni-bielefeld.de
+ *
+ */
+
+package arrayopt.layout;
+
+/**
+ * please document this
+ */
+public class MultipleAlignmentEmbedding extends ProbeEmbeddingAlgorithm
+{
+	/**
+	 * please documen this
+	 */
+	public void reembedProbeSet (Chip chip, int probe_id[])
+	{
+		reembedProbeSet (chip, probe_id, 0, probe_id.length - 1);
+	}
+
+	/**
+	 * please documen this
+	 */
+	public void reembedProbeSet (Chip chip, int probe_id[], int first, int last)
+	{
+		// to do
+	}
+
+	/**
+	 * please documen this
+	 */
+	public void reembedProbe (Chip chip, int probe_id)
+	{
+		// does nothing as there is only 1 probe to align
+	}
+}
