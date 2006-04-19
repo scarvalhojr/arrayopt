b141c31c7df8684c7508b2527249bb8314ebe743
scarvalh <>
2006-04-19 14:59:12 +0000

    Added new algorithms; added the possibility of generating a random chip instead of reading from a file; added elapsed time counter

--- src/arrayopt/textui/ArrayOpt.java
+++ src/arrayopt/textui/ArrayOpt.java
@@ -57,7 +57,7 @@ public class ArrayOpt
 		String					chip_type, filename, algorithm, dep_seq;
 		int						rows, cols, probes, probe_len, unplaced;
 		boolean					ignore_fixed;
-
+		
 		try
 		{
 			// get mandatory command-line arguments
@@ -109,6 +109,9 @@ public class ArrayOpt
 			if (algorithm.equalsIgnoreCase("RANDOM"))
 				placer = new RandomFiller();
 
+			else if (algorithm.equalsIgnoreCase("SEQUENTIAL"))
+				placer = new SequentialFiller();
+
 			else if (algorithm.equalsIgnoreCase("GREEDY10"))
 				placer = new GreedyFiller(10);
 			else if (algorithm.equalsIgnoreCase("GREEDY100"))
@@ -131,29 +134,6 @@ public class ArrayOpt
 			else if (algorithm.equalsIgnoreCase("2D4-GREEDY"))
 				placer = new TwoDimensionalPartitioning(new GreedyFiller(), 4);
 
-			/*
-			else if (algorithm.equalsIgnoreCase("PIVOT-GREEDY"))
-				placer = new PivotPartitioning(new GreedyFiller());
-
-			else if (algorithm.equalsIgnoreCase("BESTPIVOT-GREEDY"))
-				placer = new BestPivotPartitioning(new GreedyFiller());
-
-			else if (algorithm.equalsIgnoreCase("BESTPIVOT-SEQ"))
-				placer = new BestPivotPartitioning(new SequentialFiller());
-
-			else if (algorithm.equalsIgnoreCase("PREPIVOT-SEQ"))
-				placer = new PrePivotPartitioning(new SequentialFiller());
-
-			else if (algorithm.equalsIgnoreCase("IMAGINARY1-SEQ"))
-				placer = new ImaginaryChipPartitioning(new SequentialFiller(), 1);
-
-			else if (algorithm.equalsIgnoreCase("LOCALIZED7-SEQ"))
-				placer = new LocalizedPartitioning(new SequentialFiller(), 7);
-			//*/
-
-			else if (algorithm.equalsIgnoreCase("2D4-QAPBB"))
-				placer = new TwoDimensionalPartitioning(new QAPOptimization(new BranchAndBound()), 4);
-
 			else if (algorithm.equalsIgnoreCase("2D8-GREEDY"))
 				placer = new TwoDimensionalPartitioning(new GreedyFiller(), 8);
 
@@ -162,9 +142,6 @@ public class ArrayOpt
 							new QAPOptimization(
 								new GraspDense(), QAPOptimization.MODE_CONFLICT_INDEX),
 							8);
-
-			else if (algorithm.equalsIgnoreCase("QAPGRASPD"))
-				placer = new QAPOptimization(new GraspDense());
 
 			else if (algorithm.equalsIgnoreCase("LEFT"))
 				placer = new ProbeSetEmbeddingWrapper(new LeftMostEmbedding());
@@ -178,12 +155,221 @@ public class ArrayOpt
 			else if (algorithm.equalsIgnoreCase("PIVOT"))
 				placer = new ProbeSetEmbeddingWrapper(new PivotEmbedding());
 
-			else if (algorithm.equalsIgnoreCase("SWIN-GRASPD"))
-				optimizer = new SlidingWindowOptimization (
-								new QAPOptimization (
-									new GraspDense(), QAPOptimization.MODE_CONFLICT_INDEX
-								),
-								8, 8, 1);
+			else if (algorithm.equalsIgnoreCase("PRIORITY-BL-E-INC"))
+				optimizer = new PriorityReembedding(
+						OptimumSingleProbeEmbedding.BORDER_LENGTH_MIN,
+						PriorityReembedding.PRIORITY_NUM_OF_EMBEDDINGS,
+						true);
+
+			else if (algorithm.equalsIgnoreCase("PRIORITY-BL-E-TOT"))
+				optimizer = new PriorityReembedding(
+						OptimumSingleProbeEmbedding.BORDER_LENGTH_MIN,
+						PriorityReembedding.PRIORITY_NUM_OF_EMBEDDINGS,
+						false);
+
+			else if (algorithm.equalsIgnoreCase("PRIORITY-BL-N-INC"))
+				optimizer = new PriorityReembedding(
+						OptimumSingleProbeEmbedding.BORDER_LENGTH_MIN,
+						PriorityReembedding.PRIORITY_NUM_OF_NEIGHBORS,
+						true);
+
+			else if (algorithm.equalsIgnoreCase("PRIORITY-BL-N-TOT"))
+				optimizer = new PriorityReembedding(
+						OptimumSingleProbeEmbedding.BORDER_LENGTH_MIN,
+						PriorityReembedding.PRIORITY_NUM_OF_NEIGHBORS,
+						false);
+
+			else if (algorithm.equalsIgnoreCase("PRIORITY-BL-B-INC"))
+				optimizer = new PriorityReembedding(
+						OptimumSingleProbeEmbedding.BORDER_LENGTH_MIN,
+						PriorityReembedding.PRIORITY_BALANCED,
+						true);
+
+			else if (algorithm.equalsIgnoreCase("PRIORITY-BL-B-TOT"))
+				optimizer = new PriorityReembedding(
+						OptimumSingleProbeEmbedding.BORDER_LENGTH_MIN,
+						PriorityReembedding.PRIORITY_BALANCED,
+						false);
+			
+			else if (algorithm.equalsIgnoreCase("PRIORITY-CI-E-INC"))
+				optimizer = new PriorityReembedding(
+						OptimumSingleProbeEmbedding.CONFLICT_INDEX_MIN,
+						PriorityReembedding.PRIORITY_NUM_OF_EMBEDDINGS,
+						true);
+
+			else if (algorithm.equalsIgnoreCase("PRIORITY-CI-E-TOT"))
+				optimizer = new PriorityReembedding(
+						OptimumSingleProbeEmbedding.CONFLICT_INDEX_MIN,
+						PriorityReembedding.PRIORITY_NUM_OF_EMBEDDINGS,
+						false);
+
+			else if (algorithm.equalsIgnoreCase("PRIORITY-CI-N-INC"))
+				optimizer = new PriorityReembedding(
+						OptimumSingleProbeEmbedding.CONFLICT_INDEX_MIN,
+						PriorityReembedding.PRIORITY_NUM_OF_NEIGHBORS,
+						true);
+
+			else if (algorithm.equalsIgnoreCase("PRIORITY-CI-N-TOT"))
+				optimizer = new PriorityReembedding(
+						OptimumSingleProbeEmbedding.CONFLICT_INDEX_MIN,
+						PriorityReembedding.PRIORITY_NUM_OF_NEIGHBORS,
+						false);
+
+			else if (algorithm.equalsIgnoreCase("PRIORITY-CI-B-INC"))
+				optimizer = new PriorityReembedding(
+						OptimumSingleProbeEmbedding.CONFLICT_INDEX_MIN,
+						PriorityReembedding.PRIORITY_BALANCED,
+						true);
+
+			else if (algorithm.equalsIgnoreCase("PRIORITY-CI-B-TOT"))
+				optimizer = new PriorityReembedding(
+						OptimumSingleProbeEmbedding.CONFLICT_INDEX_MIN,
+						PriorityReembedding.PRIORITY_BALANCED,
+						false);
+
+			else if (algorithm.equalsIgnoreCase("SEQREEMBED-BL-INC"))
+				optimizer = new SequentialReembedding(
+								OptimumSingleProbeEmbedding.BORDER_LENGTH_MIN, true);
+			
+			else if (algorithm.equalsIgnoreCase("SEQREEMBED-CI-INC"))
+				optimizer = new SequentialReembedding(
+								OptimumSingleProbeEmbedding.CONFLICT_INDEX_MIN, true);
+
+			else if (algorithm.equalsIgnoreCase("SEQREEMBED-BL-TOT"))
+				optimizer = new SequentialReembedding(
+								OptimumSingleProbeEmbedding.BORDER_LENGTH_MIN, false);
+			
+			else if (algorithm.equalsIgnoreCase("SEQREEMBED-CI-TOT"))
+				optimizer = new SequentialReembedding(
+								OptimumSingleProbeEmbedding.CONFLICT_INDEX_MIN, false);
+
+			// ****************** GRASP Sparse ******************
+			
+			else if (algorithm.equalsIgnoreCase("GRASPS-BL"))
+				placer = new QAPOptimization (
+								new GraspSparse(), QAPOptimization.MODE_BORDER_LENGTH
+							);
+
+			else if (algorithm.equalsIgnoreCase("GRASPS-INV-BL"))
+			{
+				QAPSolverAlgorithm qapsolver = new GraspSparse();
+				qapsolver.setSwapMatrices(true);
+				placer = new QAPOptimization (
+								qapsolver, QAPOptimization.MODE_BORDER_LENGTH
+							);
+			}
+
+			else if (algorithm.equalsIgnoreCase("GRASPS-CI"))
+				placer = new QAPOptimization (
+								new GraspSparse(), QAPOptimization.MODE_CONFLICT_INDEX
+							);
+
+			else if (algorithm.equalsIgnoreCase("GRASPS-INV-CI"))
+			{
+				QAPSolverAlgorithm qapsolver = new GraspSparse();
+				qapsolver.setSwapMatrices(true);
+				placer = new QAPOptimization (
+								qapsolver, QAPOptimization.MODE_CONFLICT_INDEX
+							);
+			}
+
+			// ****************** GRASP Dense ******************
+			
+			else if (algorithm.equalsIgnoreCase("GRASPD-BL"))
+				placer = new QAPOptimization (
+								new GraspDense(), QAPOptimization.MODE_BORDER_LENGTH
+							);
+
+			else if (algorithm.equalsIgnoreCase("GRASPD-INV-BL"))
+			{
+				QAPSolverAlgorithm qapsolver = new GraspDense();
+				qapsolver.setSwapMatrices(true);
+				placer = new QAPOptimization (
+								qapsolver, QAPOptimization.MODE_BORDER_LENGTH
+							);
+			}
+
+			else if (algorithm.equalsIgnoreCase("GRASPD-CI"))
+				placer = new QAPOptimization (
+								new GraspDense(), QAPOptimization.MODE_CONFLICT_INDEX
+							);
+
+			else if (algorithm.equalsIgnoreCase("GRASPD-INV-CI"))
+			{
+				QAPSolverAlgorithm qapsolver = new GraspDense();
+				qapsolver.setSwapMatrices(true);
+				placer = new QAPOptimization (
+								qapsolver, QAPOptimization.MODE_CONFLICT_INDEX
+							);
+			}
+			
+			// ****************** GRASP Path-relinking ******************
+
+			else if (algorithm.equalsIgnoreCase("GRASPPR-BL"))
+			{
+				optimizer = new QAPOptimization (
+								new GraspPathRelinking(), QAPOptimization.MODE_BORDER_LENGTH
+							);
+			}
+
+			else if (algorithm.equalsIgnoreCase("GRASPPR-INV-BL"))
+			{
+				QAPSolverAlgorithm qapsolver = new GraspPathRelinking();
+				qapsolver.setSwapMatrices(true);
+				optimizer = new QAPOptimization (
+								qapsolver, QAPOptimization.MODE_BORDER_LENGTH
+							);
+			}
+
+			else if (algorithm.equalsIgnoreCase("GRASPPR-CI"))
+			{
+				optimizer = new QAPOptimization (
+								new GraspPathRelinking(), QAPOptimization.MODE_CONFLICT_INDEX
+							);
+			}
+
+			else if (algorithm.equalsIgnoreCase("GRASPPR-INV-CI"))
+			{
+				QAPSolverAlgorithm qapsolver = new GraspPathRelinking();
+				qapsolver.setSwapMatrices(true);
+				optimizer = new QAPOptimization (
+								qapsolver, QAPOptimization.MODE_CONFLICT_INDEX
+							);
+			}
+
+			// ****************** GRASP 128 Path-relinking ******************
+
+			else if (algorithm.equalsIgnoreCase("GRASPPR128-BL"))
+			{
+				optimizer = new QAPOptimization (
+								new GraspPathRelinking(1, 128), QAPOptimization.MODE_BORDER_LENGTH
+							);
+			}
+
+			else if (algorithm.equalsIgnoreCase("GRASPPR128-INV-BL"))
+			{
+				QAPSolverAlgorithm qapsolver = new GraspPathRelinking(1, 128);
+				qapsolver.setSwapMatrices(true);
+				optimizer = new QAPOptimization (
+								qapsolver, QAPOptimization.MODE_BORDER_LENGTH
+							);
+			}
+
+			else if (algorithm.equalsIgnoreCase("GRASPPR128-CI"))
+			{
+				optimizer = new QAPOptimization (
+								new GraspPathRelinking(1, 128), QAPOptimization.MODE_CONFLICT_INDEX
+							);
+			}
+
+			else if (algorithm.equalsIgnoreCase("GRASPPR128-INV-CI"))
+			{
+				QAPSolverAlgorithm qapsolver = new GraspPathRelinking(1, 128);
+				qapsolver.setSwapMatrices(true);
+				optimizer = new QAPOptimization (
+								qapsolver, QAPOptimization.MODE_CONFLICT_INDEX
+							);
+			}
 
 			else
 				throw new IllegalArgumentException
@@ -216,25 +402,32 @@ public class ArrayOpt
 			return;
 		}
 		
-		try
+		if (filename.equalsIgnoreCase("RANDOM"))
 		{
-			// create a file reader for the input
-			FileReader file = new FileReader(filename);
-
-			// read input
-			chip.readLayout (file, ignore_fixed);
-
-			// close reader
-			file.close();
-
+			chip.createRandomLayout();
 		}
-		catch (Exception e)
+		else
 		{
-			e.printStackTrace ();
-			System.exit(1);
-			return;
+			try
+			{
+				// create a file reader for the input
+				FileReader file = new FileReader(filename);
+	
+				// read input
+				chip.readLayout (file, ignore_fixed);
+	
+				// close reader
+				file.close();
+	
+			}
+			catch (Exception e)
+			{
+				e.printStackTrace ();
+				System.exit(1);
+				return;
+			}
 		}
-
+		
 		// make a copy of the chip before modifying its layout
 		if (chip instanceof SimpleChip)
 		{
@@ -247,16 +440,26 @@ public class ArrayOpt
 
 		if (placer != null)
 		{
+			long start = System.nanoTime();
+
 			// re-place probes on the chip
 			unplaced = placer.makeLayout(chip);
-
+			
+			long end = System.nanoTime();
+			System.err.println("Elapsed time: " + (end - start)/Math.pow(10,9) + " sec");
+			
 			if (unplaced > 0)
-				System.err.println(unplaced + " unplaced probe(s)");
+				System.err.println("WARNING: " + unplaced + " unplaced probe(s)");
 		}
-		
+
 		if (optimizer != null)
 		{
+			long start = System.nanoTime();
+			
 			optimizer.optimizeLayout (chip);
+
+			long end = System.nanoTime();
+			System.err.println("Elapsed time: " + (end - start)/Math.pow(10,9) + " sec");
 		}
 
 		if (chip.equals(copy))
@@ -269,6 +472,7 @@ public class ArrayOpt
 		else
 			System.err.println("New layout is compatible with the original specification.");
 
+		//*
 		try
 		{
 			// print chip layout
@@ -278,6 +482,7 @@ public class ArrayOpt
 		{
 			e.printStackTrace();
 		}
+		//*/
 
 		System.exit(0);
 	}
