364c6d48e75f9e1c54f631b77f1a00ff03f03eff
scarvalh <>
2006-05-27 08:50:12 +0000

    1) Implemented a fast conflict index calculation for SimpleChips; 2) Added support for sorting embeddings before the placement, although it is disabled at the moment since (it should be configured at instantiation time)

--- src/arrayopt/layout/GreedyFiller.java
+++ src/arrayopt/layout/GreedyFiller.java
@@ -37,6 +37,9 @@
 
 package arrayopt.layout;
 
+import arrayopt.util.ArrayIndexedCollection;
+import arrayopt.util.QuickSort;
+
 /**
  *
  */
@@ -51,6 +54,18 @@ public class GreedyFiller implements PlacementAlgorithm, FillingAlgorithm
 	public static final int MODE_CONFLICT_INDEX = 1;
 	
 	private boolean randomize_first;
+	
+	private RectangularRegion chip_region;
+	
+	private int embed_len;
+	
+	private int probe_len;
+	
+	private int ci_dim;
+	
+	private double pos_weight[];
+	
+	private double conflict[];
 
 	public GreedyFiller ()
 	{
@@ -122,13 +137,45 @@ public class GreedyFiller implements PlacementAlgorithm, FillingAlgorithm
 				("Only rectangular regions are supported.");
 
 		r = (RectangularRegion) region;
+		
+		// TODO sorting probes should be configurable (by the constructors)
+		// sort embeddings lexicographically (as binary strings)
+		// QuickSort.sort(new EmbeddingSort(chip, probe_id), start,end - start +1);
 
 		if (chip instanceof SimpleChip)
+		{
+			if (mode == MODE_CONFLICT_INDEX)
+			{
+				// prepare for faster conflict index calculations
+				ci_dim = ConflictIndex.dimConflictRegion();
+				chip_region = chip.getChipRegion();
+				embed_len = chip.getEmbeddingLength();
+				probe_len = chip.getProbeLength();
+				
+				if (conflict == null)
+				{
+					conflict = new double [embed_len];
+					pos_weight = new double [probe_len + 1];
+				}
+				else if (conflict.length != embed_len)
+				{
+					conflict = new double [embed_len];
+					pos_weight = new double [probe_len + 1];
+				}
+				
+				// store position weights locally
+				for (int b = 0; b <= probe_len; b++)
+					pos_weight[b] = ConflictIndex.positionWeight(b, probe_len);
+			}
+			
 			return fillRegion ((SimpleChip) chip, r, probe_id, start, end);
-
+		}
 		else if (chip instanceof AffymetrixChip)
+		{
+			// TODO implement fast conflict index calculation for Affymetrix
+			
 			return fillRegion ((AffymetrixChip) chip, r, probe_id, start, end);
-
+		}
 		else
 			throw new IllegalArgumentException ("Unsupported chip type.");
 	}
@@ -168,7 +215,7 @@ public class GreedyFiller implements PlacementAlgorithm, FillingAlgorithm
 				
 				// place selected probe
 				chip.spot[r][c] = probe_id[start];
-
+				
 				start ++;
 
 				if (start > end)
@@ -244,7 +291,7 @@ public class GreedyFiller implements PlacementAlgorithm, FillingAlgorithm
 			end = end - start + 1 > window_size ? start + window_size - 1 : end;  
 		}
 
-		min = LayoutEvaluation.borderLength (chip, row, col,probe_id[start]);
+		min = LayoutEvaluation.borderLength (chip, row, col, probe_id[start]);
 		if (min == 0) return start;
 		
 		best = start;
@@ -266,8 +313,16 @@ public class GreedyFiller implements PlacementAlgorithm, FillingAlgorithm
 	private int findMinConflictIndex (SimpleChip chip, int row, int col,
 			int probe_id[], int start, int end)
 	{
+		boolean	empty;
 		double	cost, min;
 		int		best;
+		
+		// prepare conflict index costs
+		empty = examineNeighbors (chip, row, col);
+		
+		// if region around the spot is empty,
+		// place any probe and return
+		if (empty) return start;
 
 		// if window size is limited
 		if (window_size > 0)
@@ -276,14 +331,13 @@ public class GreedyFiller implements PlacementAlgorithm, FillingAlgorithm
 			end = end - start + 1 > window_size ? start + window_size - 1 : end;  
 		}
 
-		min = LayoutEvaluation.conflictIndex (chip, row, col,probe_id[start]);
-		if (min == 0) return start;
+		min = conflictIndex (chip, probe_id[start], Double.POSITIVE_INFINITY);
 		
 		best = start;
 
 		for (int i = start + 1; i <= end; i++)
 		{
-			cost = LayoutEvaluation.conflictIndex(chip, row, col, probe_id[i]);
+			cost = conflictIndex(chip, probe_id[i], min);
 
 			if (cost < min)
 			{
@@ -293,6 +347,97 @@ public class GreedyFiller implements PlacementAlgorithm, FillingAlgorithm
 		}
 		
 		return best;
+	}
+	
+	private boolean examineNeighbors (SimpleChip chip, int row, int col)
+	{
+		boolean empty = true;
+		double delta;
+		int r, c, id, step, word, bitmask = 0;
+		
+		// reset costs
+		for (step = 0; step < embed_len; step++)
+			conflict[step] = 0;
+		
+		// define region around the spot that needs to be examined
+		int min_row = Math.max(row - ci_dim, chip_region.first_row);
+		int max_row = Math.min(row + ci_dim, chip_region.last_row);
+		int min_col = Math.max(col - ci_dim, chip_region.first_col);
+		int max_col = Math.min(col + ci_dim, chip_region.last_col);
+		
+		for (r = min_row; r <= max_row; r++)
+		{
+			for (c = min_col; c <= max_col; c++)
+			{
+				// skip if neighbor is empty
+				if ((id = chip.spot[r][c]) == Chip.EMPTY_SPOT)
+					continue;
+				
+				// get distance-dependent weight
+				delta = ConflictIndex.distanceWeight(r, c, row, col);
+				
+				if (delta == 0)
+					continue;
+				
+				empty = false;
+				
+				for (step = 0, word = - 1; step < embed_len; step++)
+				{
+					if (step % Integer.SIZE == 0)
+					{
+						word++;
+						bitmask = 0x01 << (Integer.SIZE - 1);
+					}
+					else
+						bitmask >>>= 1;
+
+					// check state of embedding at current step
+					if ((chip.embed[id][word] & bitmask) != 0)
+					{
+						// spot is in an unmasked step
+						conflict[step] += delta;
+					}
+				}
+			}
+		}
+
+		return empty;
+	}
+	
+	private double conflictIndex (SimpleChip chip, int id, double max)
+	{
+		int base, step, word, bitmask = 0;
+		double ci = 0;
+		
+		for (base = 0, step = 0, word = - 1; step < embed_len; step++)
+		{
+			if (step % Integer.SIZE == 0)
+			{
+				word++;
+				bitmask = 0x01 << (Integer.SIZE - 1);
+			}
+			else
+				bitmask >>>= 1;
+			
+			// check embedding state at current step 
+			if ((chip.embed[id][word] & bitmask) != 0)
+			{
+				// spot is in an unmasked step
+
+				// increment the number of synthesized bases
+				base++;
+			}
+			else
+			{
+				// masked step
+				ci += pos_weight[base] * conflict[step];
+				
+				// stop if CI exceeds limit
+				if (ci > max) break;
+			}
+		}
+
+		return ci;
 	}
 
 	private int findMinBorderLength (AffymetrixChip chip, int row, int col,
@@ -308,7 +453,7 @@ public class GreedyFiller implements PlacementAlgorithm, FillingAlgorithm
 			end = end - start + 1 > window_size ? start + window_size - 1 : end;  
 		}
 
-		min = LayoutEvaluation.borderLength (chip, row, col,probe_id[start]);
+		min = LayoutEvaluation.borderLength (chip, row, col, probe_id[start]);
 		if (min == 0) return start;
 		
 		best = start;
@@ -340,7 +485,7 @@ public class GreedyFiller implements PlacementAlgorithm, FillingAlgorithm
 			end = end - start + 1 > window_size ? start + window_size - 1 : end;  
 		}
 
-		min = LayoutEvaluation.conflictIndex (chip, row, col,probe_id[start]);
+		min = LayoutEvaluation.conflictIndex (chip, row, col, probe_id[start]);
 		if (min == 0) return start;
 		
 		best = start;
@@ -363,16 +508,136 @@ public class GreedyFiller implements PlacementAlgorithm, FillingAlgorithm
 	{
 		int tmp, rand;
 		
+		// TODO test this
+		
 		for (int i = start; i < end; i++)
 		{
 			// select a random element of the list
-			rand = start + (int) ((end - start + 1) * Math.random());
+			rand = i + (int) ((end - i + 1) * Math.random());
 
 			// swap the selected element with
 			// the one at the current position
-			tmp = probe_id[rand];
+			tmp = probe_id[i];
 			probe_id[i] = probe_id[rand];
 			probe_id[rand] = tmp;
 		}		
+	}
+	
+	private class EmbeddingSort implements ArrayIndexedCollection
+	{
+		private Chip chip;
+		
+		private int words;
+		
+		private int probe_id[];
+		
+		private int pivot;
+		
+		EmbeddingSort (Chip chip, int probe_id[])
+		{
+			this.chip = chip;
+			this.words = chip.embed[0].length;
+			this.probe_id = probe_id;
+		}
+		
+		public int compare (int i, int j)
+		{
+			int id_i = probe_id[i];
+			int id_j = probe_id[j];
+			
+			for (int w = 0; w < words; w++)
+			{
+				// compare first bit (signal)
+				// first bit is 1 => negative number
+				// first bit is 0 => non-negative
+				if (chip.embed[id_i][w] < 0)
+				{
+					if (chip.embed[id_j][w] >= 0)
+						return 1;
+				}
+				else
+				{
+					if (chip.embed[id_j][w] < 0)
+						return -1;
+				}
+				
+				// compare remaining bits if both have same signal
+				if (chip.embed[id_i][w] < chip.embed[id_j][w])
+					return -1;
+				else if (chip.embed[id_i][w] > chip.embed[id_j][w])
+					return 1;
+			}
+			
+			return 0;
+		}
+		
+		public void swap (int i, int j)
+		{
+			int tmp;
+			tmp = probe_id[i];
+			probe_id[i] = probe_id[j];
+			probe_id[j] = tmp;
+		}
+		
+		public void setPivot (int i)
+		{
+			this.pivot = probe_id[i];
+		}
+		
+		public int compareToPivot (int i)
+		{
+			int id_i = probe_id[i];
+
+			for (int w = 0; w < words; w++)
+			{
+				// compare first bit (signal)
+				// first bit is 1 => negative number
+				// first bit is 0 => non-negative
+				if (chip.embed[id_i][w] < 0)
+				{
+					if (chip.embed[pivot][w] >= 0)
+						return 1;
+				}
+				else
+				{
+					if (chip.embed[pivot][w] < 0)
+						return -1;
+				}
+				
+				// compare remaining bits if both have same signal
+				if (chip.embed[id_i][w] < chip.embed[pivot][w])
+					return -1;
+				else if (chip.embed[id_i][w] > chip.embed[pivot][w])
+					return 1;
+			}
+			
+			return 0;
+		}
+		
+		public int medianOfThree (int i, int j, int k)
+		{
+			int median;
+			
+			if (compare(i,j) < 0)
+			{
+				if (compare(j,k) < 0)
+					median = j;
+				else if (compare(i,k) < 0)
+					median = k;
+				else
+					median = i;
+			}
+			else
+			{
+				if (compare(j,k) > 0)
+					median = j;
+				else if (compare(i,k) > 0)
+					median = k;
+				else
+					median = i;
+			}
+			
+			return median;
+		}
 	}
 }
