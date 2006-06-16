ebeb1b475957f0525272e0e150c2c82d14133547
scarvalh <>
2006-06-16 15:45:51 +0000

    Added common file header and a reminder for a planned change in the implementation

--- src/arrayopt/util/ArrayIndexedCollection.java
+++ src/arrayopt/util/ArrayIndexedCollection.java
@@ -1,3 +1,40 @@
+/*
+ * ArrayIndexedCollection.java
+ *
+ * $Revision$
+ *
+ * $Date$
+ *
+ * Copyright 2005 Sergio Anibal de Carvalho Junior
+ *
+ * This file is part of ArrayOpt.
+ *
+ * --- License ----------------------------------------------------------------
+ * ArrayOpt is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * ÂrrayOpt is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * ArrayOpt; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307, USA.
+ * ----------------------------------------------------------------------------
+ *
+ * This is the result of a PhD work developed at the Universitaet Bielefeld
+ * under the supervision of Dr. Sven Rahmann. Proper attribution of the author
+ * as the source of the software is appreciated.
+ *
+ * Sergio Anibal de Carvalho Jr.  http://www.cebitec.uni-bielefeld.de/~scarvalh
+ * AG Genominformatik             http://gi.cebitec.uni-bielefeld.de
+ * Universitaet Bielefeld         http://www.uni-bielefeld.de
+ *
+ */
+
 package arrayopt.util;
 
 /**
@@ -9,6 +46,8 @@ package arrayopt.util;
  */
 public interface ArrayIndexedCollection
 {
+	// TODO remove medianOfThree: QuickSort should implement it using compare
+	
 	/**
 	 * Compares the elements at positions <CODE>a</CODE> and <CODE>b</CODE>. The
 	 * result must be negative if <CODE>a</CODE> is less than <CODE>b</CODE>,
--- src/arrayopt/util/QuickSort.java
+++ src/arrayopt/util/QuickSort.java
@@ -1,3 +1,40 @@
+/*
+ * QuickSort.java
+ *
+ * $Revision$
+ *
+ * $Date$
+ *
+ * Copyright 2005 Sergio Anibal de Carvalho Junior
+ *
+ * This file is part of ArrayOpt.
+ *
+ * --- License ----------------------------------------------------------------
+ * ArrayOpt is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * ÂrrayOpt is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * ArrayOpt; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307, USA.
+ * ----------------------------------------------------------------------------
+ *
+ * This is the result of a PhD work developed at the Universitaet Bielefeld
+ * under the supervision of Dr. Sven Rahmann. Proper attribution of the author
+ * as the source of the software is appreciated.
+ *
+ * Sergio Anibal de Carvalho Jr.  http://www.cebitec.uni-bielefeld.de/~scarvalh
+ * AG Genominformatik             http://gi.cebitec.uni-bielefeld.de
+ * Universitaet Bielefeld         http://www.uni-bielefeld.de
+ *
+ */
+
 package arrayopt.util;
 
 /**
@@ -39,6 +76,8 @@ public class QuickSort
 	private static final long SMALL = 7;
 
 	private static final long BIG = 41;
+	
+	// TODO implement medianOfThree using ArrayIndexedCollection.compare
 
 	/**
 	 * Sorts data indexed by an array with the QuickSort algorithm. The data
