ccdd705df36b7f22b236e61fa28b9e4ec57f3b2f
scarvalh <>
2006-12-27 14:35:17 +0000

    new and improved implementation with cleaner code and documentation

--- src/arrayopt/layout/TwoDimensionalPartitioning.java
+++ src/arrayopt/layout/TwoDimensionalPartitioning.java
@@ -38,115 +38,183 @@
 package arrayopt.layout;
 
 /**
- *
+ * This class implements the 2-dimensional Partitioning. The algorithm
+ * partitions the chip recursively based on the stated of the embeddings at a
+ * particular synthesis step.
+ * 
+ * <P>The 2-D partitioning uses the first synthesis steps to divide the probe
+ * set into sub-sets according to the state of their embeddings (masked or
+ * unmasked). The chip is divided into sub-regions proportionally to the number
+ * of masked and unmasked embeddings.</P>
+ * 
+ * <P>The partitioning of a region stops when one of the following conditions is
+ * true: 1) the number of probes in the region is less than two; 2) all
+ * synthesis steps have already been used for dividing the probe set; 3) the
+ * region dimensions (number of rows or columns) is less than {@link #stop_dim}
+ * (the stopping dimension can be specified at instantiation time; default value
+ * is given by the {@link #DEFAULT_STOP_DIMENSION} constant).</P>
+ * 
+ * <P>In the end, each sub-region is filled with its assigned probes using the
+ * {@link FillingAlgorithm} specified at instantiation time.</P>
+ * 
+ * <P>The 2-D Partitioning produces a layout in which the first masks are highly
+ * optimized, but the last masks are left with high levels of conflicts. In
+ * order to reduce conflicts in the last masks, the stopping dimension can be
+ * increased so that the filling algorithm will have more freedom on where to
+ * place the probes.</P>
+ * 
+ * <P>TODO take into account fixed spots when checking if probes fit into a
+ * partition.</P>
+ * 
+ * @author Sergio A. de Carvalho Jr.
  */
 public class TwoDimensionalPartitioning implements LayoutAlgorithm
 {
-	public static final int DEFAULT_STOP_DIMENSION = 4;
-
+	/**
+	 * Default stopping dimension. The stopping dimension is the minimum
+	 * dimension of a region (number of rows and column) necessary to perform
+	 * a partitioning.
+	 */
+	private static final int DEFAULT_STOP_DIMENSION = 4;
+	
+	/**
+	 * This constant is used to avoid partitionings of the set of probes that
+	 * results in a one very small sub-set. 
+	 */
+	private static final double MIN_DIV_RATE = .1;
+	
+	/**
+	 * Filling algorithm used to place the probes in each final sub-region.
+	 */
 	private FillingAlgorithm filler;
 
-	private int stop_dimension;
+	private int stop_dim;
 
-	private double MIN_DIV_RATE = .1;
-
+	private Chip chip;
+	
+	private int probe_id[];
+	
+	private int rows_per_probe;
+	
+	/**
+	 * Creates an instance of the 2-D Partitioning algorithm with the specified
+	 * filling algorithm.
+	 * 
+	 * @param filler filling algorithm used in the final sub-regions
+	 */
 	public TwoDimensionalPartitioning (FillingAlgorithm filler)
 	{
 		this(filler, DEFAULT_STOP_DIMENSION);
 	}
 
-	public TwoDimensionalPartitioning (FillingAlgorithm filler, int stop_dimension)
+	/**
+	 * Creates an instance of the 2-D Partitioning algorithm with the specified
+	 * filling algorithm and stopping dimension.
+	 * 
+	 * @param filler filling algorithm used in the final sub-regions
+	 * @param stop_dim stopping dimension
+	 */
+	public TwoDimensionalPartitioning (FillingAlgorithm filler, int stop_dim)
 	{
 		this.filler = filler;
-		this.stop_dimension = (stop_dimension < 1) ? 1 : stop_dimension;
+		this.stop_dim = (stop_dim < 1) ? 1 : stop_dim;
 	}
 
 	/**
-	 *
+	 * Creates a new layout of a microarray chip using the 2-D Partitioning
+	 * algorithm.
+	 * 
+	 * @param chip instance of a microarray chip
 	 */
-	public void changeLayout (Chip chip)
+	public void changeLayout (Chip c)
 	{
-		int		id[];
-		int		rows_per_probe;
-
+		RectangularRegion region;
+		
+		// save reference to chip
+		this.chip = c;
+		
 		if (chip instanceof AffymetrixChip)
 			rows_per_probe = 2;
 		else if (chip instanceof SimpleChip)
 			rows_per_probe = 1;
 		else
 			throw new IllegalArgumentException ("Unsupported chip type.");
-
+		
 		// reset current layout (if any)
 		chip.resetLayout();
 
-		// get list of movable probes
-		id = chip.getMovableProbes ();
-
-		horizontalDivide (chip, 0, chip.getChipRegion(), rows_per_probe, 0, 0,
-				id, 0, id.length - 1);
+		// get movable probes and chip region
+		this.probe_id = chip.getMovableProbes ();
+		region = chip.getChipRegion();
+		
+		horizontalDivide (region, 0, 0, 0, 0, probe_id.length - 1);
 	}
 
-	protected int horizontalDivide (Chip chip, int step, RectangularRegion r,
-		int rows_per_probe, int hor_par, int ver_par, int probe_id[],
-		int start, int end)
+	private void horizontalDivide (RectangularRegion r, int step,
+			int hpar, int vpar, int start, int end)
 	{
-		RectangularRegion	t_region, b_region;
-		int					row_div, probe_div, m_rows, u_rows, qt_cols, overflow, unplaced;
-		double				div_rate;
-
+		RectangularRegion t_region, b_region;
+		int		probe_div, overflow, m_spots, u_spots;
+		int		row_div, m_rows, u_rows, qt_cols;
+		double	div_rate;
+		
 		if (end - start + 1 < 2)
 		{
-			// insufficient number of probes for partitioning:
-			// place probes on the specified region and return
-			return filler.fillRegion (chip, r, probe_id, start, end);
+			// insufficient number of probes for partitioning
+			fillRegion (r, start, end);
+			return;
 		}
-
+		
 		if (step >= chip.getEmbeddingLength())
 		{
-			// cannot partition anymore:
-			// place probes on the specified region and return
-			return filler.fillRegion (chip, r, probe_id, start, end);
+			// no more synthesis steps to partition the probe set
+			fillRegion (r, start, end);
+			return;
 		}
-
-		if (r.last_row - r.first_row + 1 <= stop_dimension * rows_per_probe)
+		
+		if (r.last_row - r.first_row + 1 <= stop_dim * rows_per_probe)
 		{
-			if (r.last_col - r.first_col + 1 <= stop_dimension)
+			if (r.last_col - r.first_col + 1 <= stop_dim)
 			{
-				// region cannot be partitioned anymore:
-				// place probes on the specified region and return
-				return filler.fillRegion (chip, r, probe_id, start, end);
+				// region too small to be partitioned
+				fillRegion (r, start, end);
+				return;
 			}
 
-			// region can still be vertically partitined
-			return verticalDivide (chip, step, r, rows_per_probe, hor_par,
-									ver_par, probe_id, start, end);
+			// region can still be vertically partitioned
+			verticalDivide (r, step + 1, hpar, vpar, start, end);
+			return;
 		}
-
+		
 		// split the probes into two groups, M and U, according to
-		// whether they are masked or unmasked at this step
-		probe_div = divideProbes (chip, step, probe_id, start, end);
-
+		// whether they are Masked or Unmasked at the current step
+		probe_div = divideProbes (step, start, end);
+		
+		step++;
+		
 		div_rate = ((double)(probe_div - start)) / (end - start + 1);
-
-		if (div_rate < MIN_DIV_RATE || 1 - div_rate < MIN_DIV_RATE)
+		
+		if (div_rate < MIN_DIV_RATE)
 		{
-			return horizontalDivide (chip, step + 1, r, rows_per_probe,
-									hor_par, ver_par, probe_id, start, end);
+			verticalDivide (r, step, 1 - hpar, vpar, start, end);
+			return;
 		}
-
-		qt_cols = r.last_col - r.first_col + 1;
-
-		// compute how many rows will be assigned to masked and unmasked probes
+		else if (1 - div_rate < MIN_DIV_RATE)
+		{
+			verticalDivide (r, step, hpar, vpar, start, end);
+			return;
+		}
+		
+		// compute how many rows are needed to fit the masked probes
 		m_rows = (int) Math.round(div_rate * (r.last_row - r.first_row + 1));
 
-		// make sure that both partitions get a minimum number of rows
-		// (this might be useless depending on the minimum division rate)
+		// make sure partitions get a minimum number of rows
 		if (m_rows == 0)
 			m_rows += rows_per_probe;
 		else if (m_rows == r.last_row - r.first_row + 1)
 			m_rows -= rows_per_probe;
 
-		// make sure the partitions will get an appropriate number of rows
+		// make sure partitions get an appropriate number of rows
 		if (m_rows % rows_per_probe != 0)
 			if (div_rate < .5)
 			{
@@ -159,180 +227,155 @@ public class TwoDimensionalPartitioning implements LayoutAlgorithm
 
 		u_rows = (r.last_row - r.first_row + 1) - m_rows;
 
-		// check if each region will be able to handle the number of probes
-		if ((overflow = (probe_div - start) - (m_rows * qt_cols) / rows_per_probe) > 0)
-		{
+		// make sure probe sets fit into sub-regions
+		qt_cols = r.last_col - r.first_col + 1;
+		m_spots = qt_cols * m_rows / rows_per_probe;
+		u_spots = qt_cols * u_rows / rows_per_probe;
+		if ((overflow = (probe_div - start) - m_spots) > 0)
 			probe_div -= overflow;
-		}
-		else if ((overflow = (end - probe_div + 1) - (u_rows * qt_cols) / rows_per_probe) > 0)
-		{
+		else if ((overflow = (end - probe_div + 1) - u_spots) > 0)
 			probe_div += overflow;
-		}
 
-		// divide the current region into two _horizontal_ areas, T (top) and B (bottom),
-		// proportionally to the number of masked and unmasked probes
-		if (hor_par == 0)
-		{
-			// masked probes will be assigned to region T
+		// region will be divided into two sub-regions: top and bottom
+		
+		if (hpar == 0)
+			// masked probes will be assigned to top sub-region
 			row_div = r.first_row + m_rows;
-		}
 		else
-		{
-			// unmasked probes will be assigned to region T
+			// unmasked probes will be assigned to top sub-region
 			row_div = r.first_row + u_rows;
-		}
 
-		// create T (top) and B (bottom) regions
-		t_region = new RectangularRegion (r.first_row, row_div - 1, r.first_col, r.last_col);
-		b_region = new RectangularRegion (row_div, r.last_row, r.first_col, r.last_col);
+		t_region = new RectangularRegion (r.first_row, row_div - 1,
+											r.first_col, r.last_col);
+		b_region = new RectangularRegion (row_div, r.last_row,
+											r.first_col, r.last_col);
 
-		step++;
-
-		// check horizontal parity
-		if (hor_par == 0)
+		if (hpar == 0)
 		{
-			// assign masked probes to region T
-			unplaced = verticalDivide (chip, step, t_region, rows_per_probe,
-									0, ver_par, probe_id, start, probe_div - 1);
+			// assign masked probes to top sub-region
+			verticalDivide (t_region, step, 0, vpar, start, probe_div - 1);
 
-			// assign unmasked probes to region B
-			unplaced += verticalDivide (chip, step, b_region, rows_per_probe,
-										1, ver_par, probe_id, probe_div, end);
+			// assign unmasked probes to bottom sub-region
+			verticalDivide (b_region, step, 1, vpar, probe_div, end);
 		}
 		else
 		{
-			// assign masked probes to region B
-			unplaced = verticalDivide (chip, step, b_region, rows_per_probe,
-									1, ver_par, probe_id, start, probe_div - 1);
+			// assign masked probes to bottom sub-region
+			verticalDivide (b_region, step, 1, vpar, start, probe_div - 1);
 
-			// assign unmasked probes to region T
-			unplaced += verticalDivide (chip, step, t_region, rows_per_probe,
-										0, ver_par, probe_id, probe_div, end);
+			// assign unmasked probes to top sub-region
+			verticalDivide (t_region, step, 0, vpar, probe_div, end);
 		}
-
-		// return number of unplaced probes
-		return unplaced;
 	}
 
-	protected int verticalDivide (Chip chip, int step, RectangularRegion r,
-		int rows_per_probe, int hor_par, int ver_par, int probe_id[],
-		int start, int end)
+	private void verticalDivide (RectangularRegion r, int step,
+			int hpar, int vpar, int start, int end)
 	{
-		RectangularRegion	l_region, r_region;
-		int					col_div, probe_div, m_cols, u_cols, qt_rows, overflow, unplaced;
-		double				div_rate;
-
+		RectangularRegion l_region, r_region;
+		int		probe_div, overflow, m_spots, u_spots;
+		int		col_div, m_cols, u_cols, qt_rows;
+		double	div_rate;
+		
 		if (end - start + 1 < 2)
 		{
-			// insufficient number of probes for partitioning:
-			// place probes on the specified region and return
-			return filler.fillRegion (chip, r, probe_id, start, end);
+			// insufficient number of probes for partitioning
+			fillRegion (r, start, end);
+			return;
 		}
-
+		
 		if (step >= chip.getEmbeddingLength())
 		{
-			// cannot partition anymore:
-			// place probes on the specified region and return
-			return filler.fillRegion (chip, r, probe_id, start, end);
+			// no more synthesis steps to partition the probe set
+			fillRegion (r, start, end);
+			return;
 		}
-
-		if (r.last_col - r.first_col + 1 <= stop_dimension)
+		
+		if (r.last_col - r.first_col + 1 <= stop_dim)
 		{
-			if (r.last_row - r.first_row + 1 <= stop_dimension * rows_per_probe)
+			if (r.last_row - r.first_row + 1 <= stop_dim * rows_per_probe)
 			{
-				// region cannot be partitioned anymore:
-				// place probes on the specified region and return
-				return filler.fillRegion (chip, r, probe_id, start, end);
+				// region too small to be partitioned
+				fillRegion (r, start, end);
+				return;
 			}
 
-			// region can still be horizontally partitined
-			return horizontalDivide (chip, step, r, rows_per_probe,
-								hor_par, ver_par, probe_id, start, end);
+			// region can still be horizontally partitioned
+			horizontalDivide (r, step + 1, hpar, vpar, start, end);
+			return;
 		}
-
+		
 		// split the probes into two groups, M and U, according to
-		// whether they are masked or unmasked at this step
-		probe_div = divideProbes (chip, step, probe_id, start, end);
-
+		// whether they are Masked or Unmasked at the current step
+		probe_div = divideProbes (step, start, end);
+		
+		step++;
+		
 		div_rate = ((double)(probe_div - start)) / (end - start + 1);
-
-		if (div_rate < MIN_DIV_RATE || 1 - div_rate < MIN_DIV_RATE)
+		
+		if (div_rate < MIN_DIV_RATE)
 		{
-			return verticalDivide (chip, step + 1, r, rows_per_probe, hor_par,
-									ver_par, probe_id, start, end);
+			horizontalDivide (r, step, hpar, 1 - vpar, start, end);
+			return;
 		}
-
-		qt_rows = r.last_row - r.first_row + 1;
-
-		// compute how many columns are needed to the masked probes
+		else if (1 - div_rate < MIN_DIV_RATE)
+		{
+			horizontalDivide (r, step, hpar, vpar, start, end);
+			return;
+		}
+		
+		// compute how many columns are needed to fit the masked probes
 		m_cols = (int) Math.round(div_rate * (r.last_col - r.first_col + 1));
 
-		// make sure that both partitions get a minimum number of columns
-		// (this might be useless depending on the minimum division rate)
+		// make sure partitions get a minimum number of columns
 		if (m_cols == 0)
-			m_cols += rows_per_probe;
+			m_cols++;
 		else if (m_cols == r.last_col - r.first_col + 1)
-			m_cols -= rows_per_probe;
+			m_cols--;
 
 		u_cols = (r.last_col - r.first_col + 1) - m_cols;
-
-		// check if each region will be able to handle the number of probes
-		if ((overflow = (probe_div - start) - (m_cols * qt_rows) / rows_per_probe) > 0)
-		{
+		
+		// make sure probe sets fit into sub-regions
+		qt_rows = r.last_row - r.first_row + 1;
+		m_spots = m_cols * qt_rows / rows_per_probe;
+		u_spots = u_cols * qt_rows / rows_per_probe;
+		if ((overflow = (probe_div - start) - m_spots) > 0)
 			probe_div -= overflow;
-		}
-		else if ((overflow = (end - probe_div + 1) - (u_cols * qt_rows) / rows_per_probe) > 0)
-		{
+		else if ((overflow = (end - probe_div + 1) - u_spots) > 0)
 			probe_div += overflow;
-		}
 
-		// divide the current region into two _vertical_ areas, L (left) and R (right),
-		// proportionally to the number of masked and unmasked probes
-		if (ver_par == 0)
-		{
-			// masked probes will be assigned to region L
+		// region will be divided into two sub-regions: left and right
+		
+		if (vpar == 0)
+			// masked probes will be assigned to left sub-region
 			col_div = r.first_col + m_cols;
-		}
 		else
-		{
-			// unmasked probes will be assigned to region L
+			// unmasked probes will be assigned to left sub-region
 			col_div = r.first_col + u_cols;
-		}
 
-		// create L (left) and R (right) regions
-		l_region = new RectangularRegion (r.first_row, r.last_row, r.first_col, col_div - 1);
-		r_region = new RectangularRegion (r.first_row, r.last_row, col_div, r.last_col);
+		l_region = new RectangularRegion (r.first_row, r.last_row,
+											r.first_col, col_div - 1);
+		r_region = new RectangularRegion (r.first_row, r.last_row,
+											col_div, r.last_col);
 
-		step++;
-
-		// check vertical parity
-		if (ver_par == 0)
+		if (vpar == 0)
 		{
-			// assign masked probes to region L
-			unplaced = horizontalDivide (chip, step, l_region, rows_per_probe,
-								hor_par, 0, probe_id, start, probe_div - 1);
+			// assign masked probes to left sub-region
+			horizontalDivide (l_region, step, hpar, 0, start, probe_div - 1);
 
-			// assign unmasked probes to region R
-			unplaced += horizontalDivide (chip, step, r_region, rows_per_probe,
-										hor_par, 1, probe_id, probe_div, end);
+			// assign unmasked probes to right sub-region
+			horizontalDivide (r_region, step, hpar, 1, probe_div, end);
 		}
 		else
 		{
-			// assign masked probes to region R
-			unplaced = horizontalDivide (chip, step, r_region, rows_per_probe,
-								hor_par, 1, probe_id, start, probe_div - 1);
+			// assign masked probes to right sub-region
+			horizontalDivide (r_region, step, hpar, 1, start, probe_div - 1);
 
-			// assign unmasked probes to region L
-			unplaced += horizontalDivide (chip, step, l_region, rows_per_probe,
-										hor_par, 0, probe_id, probe_div, end);
+			// assign unmasked probes to left sub-region
+			horizontalDivide (l_region, step, hpar, 0, probe_div, end);
 		}
-
-		// return number of unplaced probes
-		return unplaced;
 	}
 
-	protected int divideProbes (Chip chip, int step, int probe_id[],
-		int start, int end)
+	private int divideProbes (int step, int start, int end)
 	{
 		int	p_id, w, mask;
 
@@ -363,5 +406,21 @@ public class TwoDimensionalPartitioning implements LayoutAlgorithm
 		}
 
 		return start;
+	}
+	
+	private void fillRegion (RectangularRegion r, int start, int end)
+	{
+		filler.fillRegion(this.chip, r, this.probe_id, start, end);
+	}
+
+	/**
+	 * Returns the algorithm's name together with current options.
+	 * 
+	 * @return algorithm's name and configurable options
+	 */
+	@Override
+	public String toString ()
+	{		
+		return "2DPartitioning-" + stop_dim + "-" + filler;
 	}
 }
