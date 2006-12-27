9e0520248a2d213222955cc9e0ef5433cf4ab53d
scarvalh <>
2006-12-27 14:33:59 +0000

    added a new embedding algorithm that tries to align the middle bases in the central masks as far as possible

---
+++ src/arrayopt/layout/AlignedEmbedding.java
@@ -0,0 +1,204 @@
+/*
+ * AlignedEmbedding.java
+ *
+ * $Revision$
+ *
+ * $Date$
+ *
+ * Copyright 2005 Sergio Anibal de Carvalho Junior
+ *
+ * This file is part of ArrayOpt.
+ *
+ * --- License ----------------------------------------------------------------
+ * ArrayOpt is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * ?rrayOpt is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * ArrayOpt; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307, USA.
+ * ----------------------------------------------------------------------------
+ *
+ * This is the result of a PhD work developed at the Universitaet Bielefeld
+ * under the supervision of Dr. Sven Rahmann. Proper attribution of the author
+ * as the source of the software is appreciated.
+ *
+ * Sergio Anibal de Carvalho Jr.  http://www.cebitec.uni-bielefeld.de/~scarvalh
+ * AG Genominformatik             http://gi.cebitec.uni-bielefeld.de
+ * Universitaet Bielefeld         http://www.uni-bielefeld.de
+ *
+ */
+
+package arrayopt.layout;
+
+/**
+ * This class implements the aligned re-embedding algorithm. Probes are
+ * re-embedded in such a way that the middle bases are aligned with the central
+ * masks as far as possible. Such embeddings are good when combined with a
+ * placement algorithm that optimizes the central masks such as the
+ * {@link TwoDimensionalCentralPartitioning} because the middle bases will be
+ * synthesized by masks that are highly optimized. 
+ *  
+ * @author Sergio A. de Carvalho Jr.
+ */
+public class AlignedEmbedding implements SingleProbeEmbeddingAlgorithm,
+	LayoutAlgorithm
+{
+	private LeftMostEmbedding embedder = new LeftMostEmbedding();
+	
+	/**
+	 * Creates a new layout of a microarray chip by re-embedding all probes
+	 * using the aligned re-embedding algorithm.
+	 * 
+	 * @param chip instance of a microarray chip
+	 */
+	public void changeLayout (Chip chip)
+	{
+		reembedProbeSet (chip, chip.getAllProbes());
+	}
+
+	/**
+	 * Re-embeds a set of probes of a given chip.
+	 * 
+	 * @param chip the chip containing the probes to be re-embedded
+	 * @param probe_id a list of probe IDs to be re-embedded
+	 */
+	public void reembedProbeSet (Chip chip, int probe_id[])
+	{
+		reembedProbeSet (chip, probe_id, 0, probe_id.length - 1);
+	}
+
+	/**
+	 * Re-embeds a selected sub-set of probes of a given chip. 
+	 * 
+	 * @param chip the chip containing the probes to be re-embedded
+	 * @param probe_id a list of probe IDs to be re-embedded
+	 * @param first first element on the list to be re-embedded
+	 * @param last last element on the list to be re-embedded
+	 */
+	public void reembedProbeSet (Chip chip, int probe_id[], int first, int last)
+	{
+		SimpleChip schip;
+		int embed_len, cycle_len, min_step;
+		
+		if (chip instanceof SimpleChip)
+			schip = (SimpleChip) chip;
+		else
+			throw new IllegalArgumentException ("Unsupported chip type.");
+		
+		embed_len = chip.getEmbeddingLength();
+		cycle_len = chip.depositionSequenceCycleLength();
+		
+		if (cycle_len <= 0)
+			throw new IllegalStateException
+				("Deposition sequence must be cyclical.");
+		
+		// first step of the central masks
+		min_step = (embed_len / 2) - (cycle_len / 2);
+		
+		for (int i = first; i <= last; i++)
+			reembedProbe (schip, probe_id[i], embed_len, cycle_len, min_step);
+	}
+	
+	/**
+	 * Re-embeds a single probe of a given chip. 
+	 * 
+	 * @param chip the chip containing the probe to be re-embedded
+	 * @param probe_id the ID of the probe to be re-embedded
+	 */
+	public void reembedProbe (Chip chip, int probe_id)
+	{
+		if (chip instanceof SimpleChip)
+			reembedProbe ((SimpleChip) chip, probe_id);
+		else
+			throw new IllegalArgumentException ("Unsupported chip type.");
+	}
+
+	/**
+	 * Re-embeds a single probe of a given chip. 
+	 * 
+	 * @param chip the chip containing the probe to be re-embedded
+	 * @param probe_id the ID of the probe to be re-embedded
+	 */
+	public void reembedProbe (SimpleChip chip, int probe_id)
+	{
+		int embed_len, cycle_len, min_step;
+		
+		embed_len = chip.getEmbeddingLength();
+		cycle_len = chip.depositionSequenceCycleLength();
+		
+		if (cycle_len <= 0)
+			throw new IllegalStateException
+				("Deposition sequence must be cyclical.");
+		
+		// first step of the central masks
+		min_step = (embed_len / 2) - (cycle_len / 2);
+		
+		reembedProbe (chip, probe_id, embed_len, cycle_len, min_step);
+	}
+	
+	private void reembedProbe (SimpleChip chip, int id, int embed_len,
+			int cycle_len, int min_step)
+	{
+		int free, max_shift, m, shift;
+		int pos, w, bitmask = 0;
+		
+		// leftmost embedding with no shift
+		embedder.reembedProbe(chip, id, 0);
+		
+		// count number of masked steps after last productive step
+		pos = embed_len - 1;
+		w = (int) Math.floor(pos / (double) Integer.SIZE);
+		bitmask = 0x01 << (Integer.SIZE - (embed_len % Integer.SIZE));
+		for (free = 0; pos >= 0; pos--)
+		{
+			if ((chip.embed[id][w] & bitmask) == 0)
+				free++;
+			else
+				break;
+			
+			if ((pos % Integer.SIZE) == 0)
+			{
+				bitmask = 0x01;
+				w--;
+			}
+			else
+				bitmask <<= 1;
+		}
+		
+		// compute maximum shift
+		max_shift = 4 * (int) Math.floor(free / (double) cycle_len);
+
+		// find the middle base's current position
+		m = 12;
+		
+		if (m >= min_step)
+			// with a leftmost embedding, the middle base is already past
+			// the central masks; thus it cannot be aligned
+			return;
+		
+		// compute optimal shift
+		shift = 4 * (int) Math.ceil((min_step - m) / (double) cycle_len);
+		
+		// make sure optimal shift is possible 
+		shift = shift > max_shift ? max_shift : shift;
+
+		// re-embed with shift
+		embedder.reembedProbe(chip, id, shift);
+	}
+
+	/**
+	 * Returns the algorithm's name.
+	 */
+	@Override
+	public String toString ()
+	{
+		return this.getClass().getSimpleName();
+	}
+}
